efb37fc6df862bdbf3eb981b6868f32e2b75fcb4
adakrmzts <ada.cinkilic96@hotmail.com>
2024-10-07 23:19:43 +0300

    first commit

---
+++ Patika 3. ödev/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+if (sayi<10)
+{
+    Console.WriteLine("Girdiğiniz sayı 10'dan küçük.");
+}
+else if (sayi==10)
+{
+        Console.WriteLine("Girdiğiniz sayı 10'a eşit.");
+}
+else    
+{
+        Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
+}
+if(sayi%2==0)
+{
+        Console.WriteLine("Girdiğiniz sayı çifttir.");
+}
+else
+{
+        Console.WriteLine("Girdiğiniz sayı tektir.");
+}
